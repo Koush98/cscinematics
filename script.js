2a99677a14f4daad983af83f063f7e0e99920bbb
Koush98 <144088942+Koush98@users.noreply.github.com>
2023-11-29 20:44:01 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+gsap.from("h1",{
+    scale:1.2,
+    duration:3,
+    delay:1,
+    color:"blue",
+})
+
+
+
+function toggleMenu() {
+  const menu = document.querySelector('.full');
+  menu.style.display = menu.style.display === 'none' || menu.style.display === '' ? 'block' : 'none';
+}
